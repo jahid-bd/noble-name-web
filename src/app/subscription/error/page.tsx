1e07a23402af8d046be05a08baec2d035fc25371
jahid-bd <jahidbd9x@gmail.com>
2024-04-18 00:58:43 +0600

    fix: webhook issue

---
+++ src/app/subscription/error/page.tsx
@@ -0,0 +1,9 @@
+const Error = () => {
+  return (
+    <div className="my-40">
+      <h1 className="text-center text-5xl text-red-500">Error</h1>
+    </div>
+  );
+};
+
+export default Error;
---
+++ src/app/subscription/success/page.tsx
@@ -0,0 +1,9 @@
+const Seccess = () => {
+  return (
+    <div className="my-40">
+      <h1 className="text-center text-5xl text-green-500">Success</h1>
+    </div>
+  );
+};
+
+export default Seccess;
