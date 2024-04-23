18433a3221d62f6614e2928da4af7081591373d6
jahid-bd <jahidbd9x@gmail.com>
2024-04-24 01:16:37 +0600

    feat: update profile complete page

--- src/components/form/RadioButton.tsx
+++ src/components/form/RadioButton.tsx
@@ -1,13 +1,31 @@
-const RadioButton = () => {
+interface PropTypes {
+  label: string;
+  text: string;
+  active: boolean | null;
+  onClick: () => void;
+}
+
+const RadioButton = ({ label, text, active, onClick }: PropTypes) => {
   return (
-    <div className="w-full  border border-border-primary px-4 py-[10px] flex items-center gap-2 cursor-pointer rounded-md">
-      <div className="w-4 h-4 rounded-full border border-border-primary p-[2px]">
-        <div className="bg-primary rounded-full w-full h-full"></div>
+    <button onClick={onClick} className="w-full">
+      <label
+        htmlFor={label}
+        className="font-medium text-sm text-text-secondary pb-3 block text-left"
+      >
+        {label}
+      </label>
+
+      <div className="w-full  border border-border-primary px-4 py-[10px] flex items-center gap-2 cursor-pointer rounded-md">
+        <div className="w-4 h-4 rounded-full border border-border-primary p-[2px]">
+          {active === true ? (
+            <div className="bg-primary rounded-full w-full h-full"></div>
+          ) : null}
+        </div>
+        <div>
+          <span>{text}</span>
+        </div>
       </div>
-      <div>
-        <span>Yes</span>
-      </div>
-    </div>
+    </button>
   );
 };
 
